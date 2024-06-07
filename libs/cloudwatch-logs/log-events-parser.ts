22b6badd669674b612867a71e78242836f526675
learnuidev <learnuidev@gmail.com>
2024-06-07 11:33:30 -0400

    requestid doesnt exist in init_start event. Added custom request id to make the data more consistent

--- libs/cloudwatch-logs/log-events-parser.ts
+++ libs/cloudwatch-logs/log-events-parser.ts
@@ -1,111 +1,121 @@
+const logEventParser = (event: any) => {
+  const eventMessage = event.message?.toLowerCase();
+
+  if (eventMessage?.includes("init_start")) {
+    return {
+      type: "init-start",
+      requestId: `dyno#${crypto.randomUUID()}`,
+      message: eventMessage,
+      event,
+    };
+  }
+  if (eventMessage?.includes("start")) {
+    const [requestIdRaw] = event.message
+      .split("\n")
+      .join("")
+      .split("\t")
+      .filter(Boolean);
+
+    const [, , requestId] = requestIdRaw?.split(" ");
+
+    return {
+      type: "start",
+      requestId,
+      message: eventMessage,
+      event,
+    };
+  }
+
+  if (eventMessage?.includes("report")) {
+    const [
+      requestIdRaw,
+      durationRaw,
+      billedDurationRaw,
+      memoryRaw,
+      maxMemoryUsed,
+    ] = event.message.split("\n").join("").split("\t").filter(Boolean);
+
+    const [, , requestId] = requestIdRaw?.split(" ");
+    const [, duration] = durationRaw?.split(" ");
+    const [, , billedDuration] = billedDurationRaw?.split(" ");
+    const [, , memory] = memoryRaw?.split(" ");
+    const [, , , memoryUsed] = maxMemoryUsed?.split(" ");
+    return {
+      type: "report",
+      ...event,
+      duration: duration.trim(),
+      billedDuration: billedDuration.trim(),
+      memorySize: memory.trim(),
+      memoryUsed: memoryUsed.trim(),
+      message: eventMessage,
+      requestId: requestId.trim(),
+
+      // event,
+    };
+  }
+
+  if (eventMessage?.includes("info")) {
+    const [timeStamp, requestId, info, val] = event.message
+      .split("\n")
+      .join("")
+      .split("\t")
+      .filter(Boolean);
+
+    return {
+      type: "info",
+      requestId,
+      value: val,
+      message: eventMessage,
+      ...event,
+    };
+  }
+
+  if (eventMessage?.includes("end")) {
+    const [requestIdRaw] = event.message
+      .split("\n")
+      .join("")
+      .split("\t")
+      .filter(Boolean);
+
+    const [, , requestId] = requestIdRaw?.split(" ");
+
+    return {
+      type: "end",
+      requestId,
+      message: eventMessage,
+
+      ...event,
+    };
+  }
+
+  if (eventMessage?.includes("error")) {
+    try {
+      const [time, requestId, err, errorType, errorObj] =
+        event?.message?.split("\t");
+      return {
+        type: "error",
+        message: event.message,
+
+        createdAt: time,
+        requestId: requestId.trim(),
+        errorType: errorType?.trim(),
+        errorObject: errorObj,
+        ...event,
+
+        //   event,
+      };
+    } catch (err) {
+      return {
+        type: "error",
+        message: eventMessage,
+        ...event,
+      };
+    }
+  }
+};
+
 export const logEventsParser = (events: any) => {
   return events
-    .map((event: any) => {
-      const eventMessage = event.message?.toLowerCase();
-
-      if (eventMessage?.includes("start")) {
-        const [requestIdRaw] = event.message
-          .split("\n")
-          .join("")
-          .split("\t")
-          .filter(Boolean);
-
-        const [, , requestId] = requestIdRaw?.split(" ");
-
-        return {
-          type: "start",
-          requestId,
-          message: eventMessage,
-          event,
-        };
-      }
-
-      if (eventMessage?.includes("report")) {
-        const [
-          requestIdRaw,
-          durationRaw,
-          billedDurationRaw,
-          memoryRaw,
-          maxMemoryUsed,
-        ] = event.message.split("\n").join("").split("\t").filter(Boolean);
-
-        const [, , requestId] = requestIdRaw?.split(" ");
-        const [, duration] = durationRaw?.split(" ");
-        const [, , billedDuration] = billedDurationRaw?.split(" ");
-        const [, , memory] = memoryRaw?.split(" ");
-        const [, , , memoryUsed] = maxMemoryUsed?.split(" ");
-        return {
-          type: "report",
-          ...event,
-          duration: duration.trim(),
-          billedDuration: billedDuration.trim(),
-          memorySize: memory.trim(),
-          memoryUsed: memoryUsed.trim(),
-          message: eventMessage,
-          requestId: requestId.trim(),
-
-          // event,
-        };
-      }
-
-      if (eventMessage?.includes("info")) {
-        const [timeStamp, requestId, info, val] = event.message
-          .split("\n")
-          .join("")
-          .split("\t")
-          .filter(Boolean);
-
-        return {
-          type: "info",
-          requestId,
-          value: val,
-          message: eventMessage,
-          ...event,
-        };
-      }
-
-      if (eventMessage?.includes("end")) {
-        const [requestIdRaw] = event.message
-          .split("\n")
-          .join("")
-          .split("\t")
-          .filter(Boolean);
-
-        const [, , requestId] = requestIdRaw?.split(" ");
-
-        return {
-          type: "end",
-          requestId,
-          message: eventMessage,
-
-          ...event,
-        };
-      }
-
-      if (eventMessage?.includes("error")) {
-        try {
-          const [time, requestId, err, errorType, errorObj] =
-            event?.message?.split("\t");
-          return {
-            type: "error",
-            message: event.message,
-
-            createdAt: time,
-            requestId: requestId.trim(),
-            errorType: errorType?.trim(),
-            errorObject: errorObj,
-            ...event,
-
-            //   event,
-          };
-        } catch (err) {
-          return {
-            type: "error",
-            message: eventMessage,
-            ...event,
-          };
-        }
-      }
-    })
-    ?.sort((a: any, b: any) => a?.timestamp - b?.timestamp);
+    .map(logEventParser)
+    ?.sort((a: any, b: any) => b?.eventIdf - a?.eventId);
 };
